4b5cb197fa8430ceda556ca5ec156c8ce530b4c4
kubkowski <jakub.opara@gmail.com>
2015-03-18 11:41:55 +0100

    New URL, view and template to display lists

--- superlists/lists/tests.py
+++ superlists/lists/tests.py
@@ -66,14 +66,19 @@ class ItemModelTest(TestCase):
         self.assertEqual(second_saved_item.text, 'Item the second')
 
 class ListViewTest(TestCase):
-    def test_home_page_displays_all_list_items(self):
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
+    def test_displays_all_items(self):
         Item.objects.create(text='itemey 1')
         Item.objects.create(text='itemey 2')
 
-        response = self.client.get('/lists/the-only-list-in-the-world')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
 
-        self.assertIn('itemey 1', response.content.decode())
-        self.assertIn('itemey 2', response.content.decode())
+        self.assertContains(response, 'itemey 1')
+        self.assertContains(response, 'itemey 2')
 
 
 
