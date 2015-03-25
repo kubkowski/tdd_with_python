6431682f34e978092bb1f7f4634613605a9b44f0
kubkowski <jakub.opara@gmail.com>
2015-03-25 15:34:03 +0100

    Use Bootstrap to improve layout

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,8 +1,8 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -100,7 +100,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta = 5
+            delta=5
         )
 
         # She starts a new list and sees the input is nicely
